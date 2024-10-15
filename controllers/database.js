e19c4acd3e70b2dea878f5b095221ab90596cfe0
Elvis Kemoi <elviskemoi001@gmail.com>
2024-10-15 13:32:36 +0300

    Fixed Database

--- controllers/database.js
+++ controllers/database.js
@@ -1,20 +1,5 @@
 const mongoose = require("mongoose");
-<<<<<<< HEAD
-require("dotenv").config();
 
-const connectToDb = async () => {
-	// Check if the Mongoose connection is already established
-	if (mongoose.connection.readyState === 1) {
-		// 1 = connected
-		console.log("Using existing MongoDB connection");
-		return;
-	}
-	try {
-		await mongoose.connect(process.env.MONGODB_URI, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
-=======
 const env = require("dotenv").config();
 
 let isConnected = false; // Track connection state
@@ -26,7 +11,7 @@ const connectToDb = async () => {
 	try {
 		await mongoose.connect(process.env.MONGODB_URI);
 		isConnected = true; // Connection successful
->>>>>>> 99a76c2d0dd63976a5897441fe00b9ffa72e3e19
+
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.error("Error connecting to MongoDB", error);
